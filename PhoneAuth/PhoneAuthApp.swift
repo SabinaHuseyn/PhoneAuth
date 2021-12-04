9c36f5e7cb5894185de307235a6458dc184ff560
SabinaHuseyn <sabinah3377@gmail.com>
2021-12-05 02:20:00 +0400

    Initial commit

---
+++ PhoneAuth/PhoneAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneAuthApp.swift
+//  PhoneAuth
+//
+//  Created by Sabina Huseynova on 05.12.21.
+//
+
+import SwiftUI
+
+@main
+struct PhoneAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
